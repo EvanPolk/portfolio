31946060e7011074c2684bbd084989fce04ae529
EvanPolk <epolk@luc.edu>
2024-12-04 12:58:12 -0600

    Sorted out organizing and added photo

--- src/components/Descriptor.tsx
+++ src/components/Descriptor.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Profile from '../assets/ProfessionalProfilePicture.png';
 
 function Descriptor() {
   return (
-    <div className='h-32 w-auto flex justify-end'>
+    <div className='h-64 w-full flex justify-center items-center'>
+      <img
+        className='w-fit h-56 border-tertiary border-2 aspect-square object-cover object-top rounded-lg'
+        src={Profile}
+      ></img>
       <div className='text-black m-4 w-64'>
         <h2 className='text-black text-3xl font-bold'>Evan Polk</h2>
         <h2 className='text-xl font-bold'>Fullstack Engineer</h2>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import About from './pages/About';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<About />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
