983eaa61f9bffffe6d093023aa13cf9c2c96d55d
EvanPolk <epolk@luc.edu>
2024-12-28 13:48:57 -0600

    Figured out public s3 bucket hosting, now moving toward hosting via CDN

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,41 +1,84 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.82.2"
-    }
+locals {
+  dist_path = "../frontend/dist"
+  files     = fileset(local.dist_path, "**/*")
+  domain    = "evan-polk.com"
+}
+
+# Bucket Configuration
+resource "aws_s3_bucket" "main" {
+  bucket = "evan-polk-portfolio-bucket"
+}
+
+resource "aws_s3_bucket_website_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  index_document {
+    suffix = "index.html"
   }
 }
 
-provider "aws" {
-  region = "us-east-2"
+resource "aws_s3_object" "main" {
+  for_each = toset(local.files)
+
+  bucket = aws_s3_bucket.main.id
+  key    = each.value
+  source = "${local.dist_path}/${each.value}"
+
+  content_type = lookup({
+    "html" = "text/html",
+    "css"  = "text/css",
+    "js"   = "application/javascript",
+    "json" = "application/json",
+    "jpg"  = "image/jpeg",
+    "jpeg" = "image/jpeg",
+    "png"  = "image/png",
+    "gif"  = "image/gif",
+    "svg"  = "image/svg+xml"
+  }, split(".", "${each.value}")[1], "text/html")
 }
 
-resource "aws_s3_bucket" "bucket-portfolio" {
-  bucket = "evanpolk-us-east-2-bucket-static"
+resource "aws_s3_bucket_ownership_controls" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.bucket-portfolio.id
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
 
+  # TODO: Make bucket private for CDN
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.bucket-portfolio.id
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.main.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.bucket-portfolio.arn}/*"
-      }
-    ]
-  })
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        # Allow public read access to the bucket (list the contents)
+        {
+          "Sid" : "PublicReadListBucket",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "s3:ListBucket",
+          "Resource" : "${aws_s3_bucket.main.arn}"
+        },
+        # Allow public read access to all objects inside the bucket
+        {
+          "Sid" : "PublicReadGetObject",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "s3:GetObject",
+          "Resource" : "${aws_s3_bucket.main.arn}/*"
+        }
+      ]
+    }
+  )
 }
