60ea96f23fc53624dbed56cd26916475adf3951b
Evan Polk <evanpolk2@gmail.com>
2025-07-06 14:48:07 -0500

    Finished filling out basic website info, Publishing this version!

--- frontend/src/pages/AboutMe.tsx
+++ frontend/src/pages/AboutMe.tsx
@@ -1,10 +1,69 @@
 import Navbar from '../components/Navbar';
+import LinkedIn from '../assets/brand-linkedin.svg';
+import Github from '../assets/brand-github.svg';
+import { Link } from 'react-router';
 
 function AboutMe() {
   return (
-    <div className='w-screen h-screen font-dmsans bg-stone-300 overflow-visible'>
+    <div className='w-screen h-fill font-dmsans bg-stone-300 overflow-visible'>
       <Navbar startPage='About-Me'/>
-      
+      <div className='w-screen h-1/2 flex-col items-center justify-center text-stone-800'>
+        <h1 className='px-8 pt-8 font-bold text-3xl'>A little about me</h1>
+        <div>
+          <h1 className='px-8 pt-4 flex justify-center items-center'>Education</h1>
+          <div className='h-0.5 w-fill mx-8 mt-2 bg-stone-800'></div>
+          <h1 className='px-8 pt-4'><span className='font-bold'>B.S. in Computer Science</span> at Loyola University Chicago</h1>
+        </div>
+        <div>
+          <h1 className='px-8 pt-4 flex justify-center items-center'>Experience</h1>
+          <div className='h-0.5 w-fill mx-8 mt-2 bg-stone-800'></div>
+          <h1 className='px-8 pt-4'>
+            <span className='text-md flex justify-center items-center'>May 2025 - Present</span>
+            <span className='font-bold'>ITS Service Desk Student Technician</span> at LUC
+          </h1>
+          <ol className='pl-10 pr-8 pt-4'>
+            <div className='flex flex-row pt-2'>
+              <div className='h-1.5 aspect-square m-2 rounded-full bg-stone-800'></div>
+              <h1>Managed over 100,000 alumni, faculty, staff, and students through Microsoft Azure</h1>
+            </div>
+            <div className='flex flex-row pt-2'>
+              <div className='h-1.5 aspect-square m-2 rounded-full bg-stone-800'></div>
+              <h1>Communicated with clients, both over the phone and through email, to troubleshoot technical issues and escalate service requests.</h1>
+            </div>
+            <div className='flex flex-row pt-2'>
+              <div className='h-1.5 aspect-square m-2 rounded-full bg-stone-800'></div>
+              <h1>Learned common troubleshooting techniques for over 150 university services including Microsoft 365, Global Protect VPN, and Microsoft Authenticator.</h1>
+            </div>
+          </ol>
+
+          <h1 className='px-8 pt-4'>
+            <span className='text-md flex justify-center items-center'>Aug. 2024 - May 2025</span>
+            <span className='font-bold'>Computer Science Peer Tutor</span> at LUC
+          </h1>
+          <ol className='pl-10 pr-8 pt-4'>
+            <div className='flex flex-row pt-2'>
+              <div className='h-1.5 aspect-square m-2 rounded-full bg-stone-800'></div>
+              <h1>Communicated with both students and professors to help develop better methods of tutoring course material.</h1>
+            </div>
+            <div className='flex flex-row pt-2'>
+              <div className='h-1.5 aspect-square m-2 rounded-full bg-stone-800'></div>
+              <h1>Provided instruction to students from diverse backgrounds, ranging from beginners with minimal experience to those with solid or advanced proficiency.</h1>
+            </div>
+            <div className='flex flex-row pt-2 pb-2'>
+              <div className='h-1.5 aspect-square m-2 rounded-full bg-stone-800'></div>
+              <h1>Monitored and assessed student progress, providing individualized feedback and tailored support.</h1>
+            </div>
+          </ol>
+        </div>
+
+        <div>
+          <h1 className='px-8 py-4'>Want to learn more? Reach out through LinkedIn or Explore my Github!</h1>
+          <div className='w-full h-fit px-8 pb-4 flex flex-row items-center'>
+            <a href='https://www.linkedin.com/in/evan-polk-6547412a0/' target='_blank'><img src={LinkedIn} className='h-8 pr-2'></img></a>
+            <a href='https://www.github.com/EvanPolk' target='_blank'><img src={Github} className='h-8 px-2'></img></a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Homelab.tsx
+++ frontend/src/pages/Homelab.tsx
@@ -2,9 +2,44 @@ import Navbar from '../components/Navbar';
 
 function Homelab() {
   return (
-    <div className='w-screen h-screen font-dmsans bg-stone-300 overflow-visible'>
+    <div className='w-screen h-fill font-dmsans bg-stone-300 overflow-visible'>
       <Navbar startPage='HomeLab'/>
-      
+      <div className='w-screen h-fill flex-col items-center justify-center text-stone-800'>
+        <div>
+          <h1 className='px-8 pt-8 text-xl font-semibold'>Summary</h1>
+          <h1 className='px-8 pt-4'>My interest in homelabbing was largely what brought me to consider doing IT work as a career. I really enjoyed applying my education to real world problems to actively made my life easier.</h1>
+        </div>
+
+        <div>
+          <h1 className='px-8 pt-8 text-xl font-semibold'>Services</h1>
+          <div className='px-8 flex flex-row items-center'>
+            <div className='h-1.5 aspect-square m-2 rounded-full bg-stone-800'></div>
+            <h1 className='pt-4'>Tailscale - VPN service which lets me access the benefits of my home lab from anywhere</h1>
+          </div>
+          <div className='px-8 flex flex-row items-center'>
+            <div className='h-1.5 aspect-square m-2 rounded-full bg-stone-800'></div>
+            <h1 className='pt-4'>Samba - Allowed me to move away from cloud storage solutions and save on costs</h1>
+          </div>
+          <div className='px-8 flex flex-row items-center'>
+            <div className='h-1.5 aspect-square m-2 rounded-full bg-stone-800'></div>
+            <h1 className='pt-4'>Apache - This is the web server I chose to distribute my website</h1>
+          </div>
+          <div className='px-8 flex flex-row items-center'>
+            <div className='h-1.5 aspect-square m-2 rounded-full bg-stone-800'></div>
+            <h1 className='pt-4'>Pihole - Helps me create domains for internal services that aren't public</h1>
+          </div>
+          <div className='px-8 flex flex-row items-center'>
+            <div className='h-1.5 aspect-square m-2 rounded-full bg-stone-800'></div>
+            <h1 className='pt-4'>Nginx Proxy Manager - This allows me to expose internal web services through TLS encrypted https urls instead of using http://homeserver:port for every internal service</h1>
+          </div>
+        </div>
+
+        <div>
+          <h1 className='px-8 pt-8 text-xl font-semibold'>Future</h1>
+          <h1 className='px-8 pt-4'>I'm moving into my new apartment soon. With this improvement in housing I also get to control my own router settings on my own wifi network. This will allow me to switch from exclusively using docker containers to using proxmox.</h1>
+          <h1 className='px-8 py-4'> I plan on spinning up VMs for my webserver and pihole services. My huge Docker-Compose file will be on its own VM as well. This will help simplify and speed up my network. Currently due to port forwarding and network discovery limitations on my universities wifi, I was unable to make this switch.</h1>
+        </div>
+      </div>
     </div>
   );
 }
